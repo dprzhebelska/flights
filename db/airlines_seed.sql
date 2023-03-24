41e544ea89a49a96cc1d20b6aadff39b2912cf24
dprzhebelska <daria.sergiyovna@gmail.com>
2023-03-24 19:47:33 -0400

    add db

---
+++ db/airlines_seed.sql
@@ -0,0 +1,8 @@
+INSERT INTO airlines VALUES ('American Airlines',1930,350,900,'AA','AAL','AMERICAN','Yes',17.3,133700,956,'Fort Worth','Texas'),
+	('Delta',1924,325,800,'DL','DAL','DELTA','Yes',47,91000,850,'Atlanta','Georgia'),
+	('Frontier Airlines',1994,100,100,'F9','FFT','FRONTIER FLIGHT','Yes',2.5,4000,100,'Denver','Colorado'),
+	('JetBlue Airways',1998,100,250,'B6','JBU','JETBLUE','Yes',8.1,22000,270,'Long Island City','New York'),
+	('Spirit Airlines',1980,70,150,'NK','NKS','SPIRIT','Yes',3.3,9000,160,'Miramar','Florida'),
+	('Sun Country Airlines',1982,50,30,'SY','SCX','SUN COUNTRY','Yes',1.1,2000,30,'Eagan','Minnesota'),
+	('Alaska Airlines',1932,115,330,'AS','ASA','ALASKA','Yes',8.4,21000,330,'Seattle','Washington'),
+	('United Airlines',1905,350,792,'UA','UAL','UNITED','Yes',15.4,59000,872,'Chicago','Illinois');
